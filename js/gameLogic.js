059da00ce4e4de19222f2cd2f1391b9708fd5c55
jess-isaacs <jessie.isaacs14@gmail.com>
2025-02-13 16:43:24 -0600

    added files

---
+++ js/gameLogic.js
@@ -0,0 +1,76 @@
+console.log('hi');
+
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'scissors' || userInput === 'paper' || userInput === 'bomb') {
+    return userInput;
+  } else {
+    console.log("Error. Not a valid choice. Please select rock, paper, or scissors");
+  }
+};
+
+const getComputerChoice = () => {
+ let randomNum = Math.floor(Math.random() * 3);
+
+ if (randomNum === 0) {
+  return 'rock';
+ } else if (randomNum === 1) {
+  return 'paper';
+ } else if (randomNum === 2) {
+  return 'scissors';
+ } else {
+  console.log("Error with randomNum generation");
+ }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+
+  if (userChoice === 'bomb') {
+    return "user wins";
+  }
+
+  if (userChoice === computerChoice) {
+    return "tie";
+  }
+
+  if (userChoice === 'rock') {
+    if (computerChoice === 'scissors') {
+      return "user wins";
+    } else {
+      return "computer wins";
+    }
+  }
+
+  if (userChoice === 'paper') {
+    if (computerChoice === 'rock') {
+      return "user wins";
+    } else {
+      return "computer wins";
+    }
+  }
+
+  if (userChoice === 'scissors') {
+    if (computerChoice === 'paper') {
+      return "user wins";
+    } else {
+      return "computer wins";
+    }
+  }
+};
+
+//TESTING IT OUT:
+  // Store user's choice
+ // Store computer's choice
+ // Now we're passing pre-stored values
+
+const playGame = () => {
+  const userChoice = getUserChoice('Bomb');
+  const computerChoice = getComputerChoice(); 
+
+  console.log(`User choice: ${userChoice}`);
+  console.log(`Computer choice: ${computerChoice}`);
+
+  console.log(determineWinner(userChoice,computerChoice));
+};
+
+playGame();
